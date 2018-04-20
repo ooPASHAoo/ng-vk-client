50638d181fbb9c3c8f8b834f9f0c325cfeab1332
ooPASHAoo <garskiy.pavel@gmail.com>
2018-04-21 02:00:11 +0300

    Add: page/api groups (unfinished)

---
+++ src/app/core/vk-api/methods/services/vk-api-groups.service.ts
@@ -0,0 +1,36 @@
+import {Injectable} from '@angular/core';
+import {HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {map} from 'rxjs/operators';
+
+import {VkApiServiceAbstract} from './vk-api.service.abstract';
+import {Stp} from '../../../../shared/supports/safe-type-parser';
+import {VkGroup} from '../models/vk-group.model';
+
+@Injectable()
+export class VkApiGroupsService extends VkApiServiceAbstract {
+
+  protected METHOD_URL = 'groups.get';
+
+  /** @override */
+  protected getDefaultParams(): HttpParams {
+    return super.getDefaultParams()
+      .set('extended', '1');
+  }
+
+  getByUserId(userId: string): Observable<VkGroup[]> {
+    const params = this.getDefaultParams()
+      .set('user_id', userId);
+
+    return this.httpJsonpGet(params).pipe(
+      map(VkApiGroupsService._parseResponse)
+    );
+  }
+
+  private static _parseResponse(res: object): VkGroup[] {
+    console.log('- PG:', res);
+    return Stp.getIsHas(res, ['items'], Array, true)
+      .map(VkGroup.parseItem);
+  }
+
+}
---
+++ src/app/pages/main/pages/user/user-content/groups/groups.component.ts
@@ -0,0 +1,81 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {VkGroup} from '../../../../../../core/vk-api/methods/models/vk-group.model';
+import {VkApiGroupsService} from '../../../../../../core/vk-api/methods/services/vk-api-groups.service';
+import {ApiError} from '../../../../../../core/vk-api/methods/errors/api-error';
+import {AuthVkError} from '../../../../../../core/vk-api/methods/errors/token-error';
+import {StpError} from '../../../../../../shared/supports/safe-type-parser';
+
+@Component({
+  selector: 'pg-groups',
+  templateUrl: './groups.component.html',
+  styleUrls: ['./groups.component.scss']
+})
+export class GroupsComponent implements OnInit {
+
+  userId: string;
+
+  groupsList: VkGroup[];
+  isLoaded = false;
+  hasLoadError = false;
+
+  constructor(private _activatedRoute: ActivatedRoute,
+              private _vkApiGroups: VkApiGroupsService,
+              private _router: Router) {
+  }
+
+  ngOnInit() {
+    this._activatedRoute.parent.paramMap
+      .subscribe((paramMap) => {
+        this.userId = paramMap.get('id');
+        this._refresh();
+      });
+  }
+
+  // --- actions --- //
+
+  onRefresh() {
+    this._refresh();
+  }
+
+  // --- private --- //
+
+  private _refresh() {
+    this._loadGroupList(this.userId);
+  }
+
+  private _loadGroupList(userId: string) {
+    this.isLoaded = false;
+    this.hasLoadError = false;
+
+    this._vkApiGroups.getByUserId(userId)
+      .subscribe(
+        this._responseSuccessHandler.bind(this),
+        this._responseFailureHandler.bind(this)
+      );
+  }
+
+  private _responseSuccessHandler(res: VkGroup[]) {
+    this.isLoaded = true;
+    this.groupsList = res;
+  }
+
+  private _responseFailureHandler(err: ApiError|AuthVkError|Error) {
+    console.warn(`- PG: ${err.name} - ${err.message}`);
+    if (err instanceof StpError) {
+      console.dir(err.parseObject);
+    }
+
+    this.hasLoadError = true;
+
+    if (err instanceof AuthVkError) {
+      alert(err.userDescription);
+      this._router.navigate([err.loginRoute]);
+    } else {
+      alert('Ошибка при загрузке списка групп. Попробуйте еще раз.');
+    }
+
+    this.isLoaded = true;
+  }
+
+}
