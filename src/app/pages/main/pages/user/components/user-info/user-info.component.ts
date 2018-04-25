6982620ce8bd4c4b0a7f44870b9c553c2d126ebc
ooPASHAoo <garskiy.pavel@gmail.com>
2018-04-26 01:03:11 +0300

    Add: avatar load animate

--- src/app/pages/main/pages/user/components/user-info/user-info.component.ts
+++ src/app/pages/main/pages/user/components/user-info/user-info.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 
 import {VkUser} from '../../../../../../core/vk-api/methods/models/vk-user.model';
 import {fadeInOutAnimation} from '../../../../../../shared/animations/fade-in-out.animation';
@@ -10,13 +10,24 @@ import {fadeInOutAnimation} from '../../../../../../shared/animations/fade-in-ou
   styleUrls: ['./user-info.component.scss'],
   animations: [fadeInOutAnimation]
 })
-export class UserInfoComponent implements OnInit {
+export class UserInfoComponent implements OnInit, OnChanges {
 
   @Input() user: VkUser;
   @Input() errorMsg: string;
+
+  isLoadedAvatar = false;
 
   constructor() {}
 
   ngOnInit() {}
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['user']) {
+      this.isLoadedAvatar = false;
+    }
+  }
+
+  avatarHasLoaded() {
+    this.isLoadedAvatar = true;
+  }
 }
